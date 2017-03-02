70d3e2d435f52d5bfe05d6cb6b7cccbe72c50f54
YDD9 <hushuai2003cn@hotmail.com>
2017-03-02 22:03:17 +0100

    Create L2OddOccuranceInArray.py

---
+++ L2OddOccuranceInArray.py
@@ -0,0 +1,13 @@
+## [http://www.ardendertat.com/2011/12/13/programming-interview-questions-22-find-odd-occurring-element/]
+
+def getOdd(arr):
+    return reduce(lambda x, y: x^y, arr)
+
+## reduce (function, iterable [, initializer])
+## http://python-reference.readthedocs.io/en/latest/docs/functions/reduce.html?highlight=reduce
+
+## advanced case find two odd occurance
+## Find the two numbers with odd occurrences in an unsorted array
+
+## http://www.geeksforgeeks.org/find-the-two-numbers-with-odd-occurences-in-an-unsorted-array/
+## http://stackoverflow.com/questions/22952651/explain-using-xor-to-find-two-non-duplicate-integers-in-an-array
