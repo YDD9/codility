23a074f1da2ca9c965df895eea1f7d27c57c57e7
YDD9 <hushuai2003cn@hotmail.com>
2017-03-20 00:36:32 +0100

    Update L4MaxCounter.py

--- L4MaxCounter.py
+++ L4MaxCounter.py
@@ -18,3 +18,18 @@ def solution(N, A):
 ## add oldmaxcount, if oldmaxcount == maxcount, no new res will be created.
 
 ## I think i picked up some ideas from other languages, find my solution. The key is not to iterate/create new array of results in the loop over A. just keep track of the last maximum. only AFTER the loop completes - adjust values less than last maximum - with the last maximum
+
+def solution(N, A):
+    # write your code in Python 2.7
+    M = len(A)
+    res = [0] * N
+    maxcount = 0
+    sumcount = 0
+    for K in range(M):
+        if 1 <= A[K] <= N:
+            res[A[K]-1] += 1
+            if res[A[K]-1] >= maxcount:
+                maxcount = res[A[K]-1]                
+        if A[K] == N + 1:
+            sumcount += maxcount 
+    return res + [sumcount] * N
